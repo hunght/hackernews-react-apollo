aab23ceb9b58f5d4a21e208991df6c191441e5a9
Hoang Hung <hth321@gmail.com>
2018-03-20 17:51:03 +0700

    fix update mutation

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,6 +4,18 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    // 1
+    const data = store.readQuery({ query: FEED_QUERY });
+
+    // 2
+    const votedLink = data.feed.links.find(link => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    // 3
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
+
   render() {
     // 1
     if (this.props.feedQuery && this.props.feedQuery.loading) {
@@ -19,12 +31,21 @@ class LinkList extends Component {
     const linksToRender = this.props.feedQuery.feed.links;
 
     return (
-      <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
+      <div>
+        {linksToRender.map((link, index) => (
+          <Link
+            key={link.id}
+            updateStoreAfterVote={this._updateCacheAfterVote}
+            index={index}
+            link={link}
+          />
+        ))}
+      </div>
     );
   }
 }
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   # 2
   query FeedQuery {
     feed {
@@ -33,6 +54,16 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
